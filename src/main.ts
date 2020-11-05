889be9e8ba1c7cebd5d10e242757436bfcea9cae
yee2542 <yee2542gtl@gmail.com>
2020-11-05 20:53:37 +0700

    Implement config to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
 import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 import { AppModule } from './app.module';
+import { ConfigurationInterface } from './config/config.interface';
+import { ConfigModule } from './config/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,11 @@ async function bootstrap() {
     .setTitle('CPE KMUTT API')
     .setVersion('1.0')
     .build();
+
+  const config: ConfigurationInterface = app
+    .select(ConfigModule)
+    .get('APP_CONFIG');
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
