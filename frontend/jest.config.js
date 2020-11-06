75775bb576faac4ecff3f1aee7cde88e2ae51d35
yee2542 <yee2542gtl@gmail.com>
2020-11-06 16:51:02 +0700

    Setup jest enzyme

---
+++ frontend/jest.config.js
@@ -0,0 +1,38 @@
+// module mapper hotfix
+const tsconfig = require("./tsconfig.json");
+const aliasModule = Object.keys(tsconfig.compilerOptions.paths)
+  .map((alias) => {
+    const value = tsconfig.compilerOptions.paths[alias];
+    return {
+      // alias
+      ["^" + alias.replace("*", "(.*)")]:
+        // absolute path
+        "<rootDir>/" + String(value).replace("*", "") + "$1",
+    };
+  })
+  .reduce((acc, cur) => {
+    return { ...acc, ...cur };
+  }, {});
+
+module.exports = {
+  // roots: ["src"],
+  moduleDirectories: ["./src", "node_modules"],
+  testPathIgnorePatterns: ["<rootDir>/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "identity-obj-proxy",
+    ...aliasModule,
+  },
+
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+      babelConfig: true,
+    },
+  },
+};
