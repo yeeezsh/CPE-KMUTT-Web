2260ab699d87448e0538163668ba25e42f8b8d68
UnkAtreus <nom_noon00548@hotmail.com>
2020-11-12 17:41:25 +0700

    fix defult import

--- frontend/src/common/components/Container/styled.ts
+++ frontend/src/common/components/Container/styled.ts
@@ -1,10 +1,10 @@
+import BREAKPOINTS from 'src/common/constants/breakpoints';
 import styled from 'styled-components';
-import { default as VARIABLES } from '../../utils';
 
 export const Containers = styled.div`
   display: flex;
   width: 100%;
-  max-width: ${VARIABLES.BREAKPOINTS.IPAD_LANDSCAPE};
+  max-width: ${BREAKPOINTS.IPAD_LANDSCAPE};
   margin: 0 auto;
   flex-wrap: wrap;
 `;
--- frontend/src/common/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import { default as BREAKPOINTS } from './breakpoints';
-import { default as COLORS } from './colors';
-
-export default {
-  BREAKPOINTS,
-  COLORS,
-};
