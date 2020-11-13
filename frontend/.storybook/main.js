2e0a6a09d4c89086816b9894fff943c8c1ca337c
yee2542 <yee2542gtl@gmail.com>
2020-11-13 22:26:48 +0700

    Fix alias path prob

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,4 +1,10 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
 };
