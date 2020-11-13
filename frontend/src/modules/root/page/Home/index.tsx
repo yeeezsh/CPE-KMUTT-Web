738ebebb8a7bc880931baa195a45d2d5dc6e4bff
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2020-11-13 15:24:21 +0700

    Update home with navbar

--- frontend/src/modules/root/page/Home/index.tsx
+++ frontend/src/modules/root/page/Home/index.tsx
@@ -1,21 +1,25 @@
+import Navbar from 'modules/root/components/Navbar/index';
 import Head from 'next/head';
 import React from 'react';
 import { Container, Main, Title } from './styled';
 
 const Home: React.FC = () => {
   return (
-    <Container>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Navbar />
+      <Container>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Title>
-      </Main>
-    </Container>
+        <Main>
+          <Title>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </Title>
+        </Main>
+      </Container>
+    </>
   );
 };
 
