cbd1878d5e5938f7e99defc9ca26c59cd6d634d6
Krittaphon Raksakul <krittaphon.official@gmail.com>
2020-11-14 23:54:50 +0700

    Fix navbar

--- frontend/src/modules/root/components/Navbar/index.tsx
+++ frontend/src/modules/root/components/Navbar/index.tsx
@@ -1,47 +1,50 @@
 import { CloseOutlined, MenuOutlined } from '@ant-design/icons';
 import { Dropdown } from 'antd';
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import NavbarMenu from './NavbarMenu';
 import {
   Department,
   Faculty,
-  Logo,
+  Li,
   LogoCPE,
   LogoKMUTT,
   MenuButton,
   Nav,
+  NavbarBand,
   StyledDivider,
 } from './styled';
 
 const Navbar: React.FC = () => {
   const [visible, setVisible] = useState(false);
 
-  const showDrawer = useCallback(() => setVisible(!visible), [visible, setVisible]);
+  const showDropdown = () => setVisible((value) => !value);
 
   return (
     <>
       <Nav>
-        <Logo>
-          <LogoKMUTT src="/LOGO-KMUTT.svg" />
-          <StyledDivider type="vertical" />
-          <LogoCPE src="/LOGO-CPE.svg" />
-          <Faculty>
-            ภาควิชาวิศวกรรมคอมพิวเตอร์
-            <Department>
-              คณะวิศวกรรมศาสตร์มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
-            </Department>
-          </Faculty>
-        </Logo>
-        <MenuButton> {visible ? '​​  ' : 'EN'}</MenuButton>
-        <Dropdown
-          overlay={NavbarMenu}
-          placement="bottomCenter"
-          trigger={['click']}
-          overlayStyle={{ width: '100%' }}>
-          <MenuButton onClick={showDrawer}>
-            {visible ? <CloseOutlined /> : <MenuOutlined />}
-          </MenuButton>
-        </Dropdown>
+        <Li>
+          <NavbarBand>
+            <LogoKMUTT src="/LOGO-KMUTT.svg" />
+            <StyledDivider type="vertical" />
+            <LogoCPE src="/LOGO-CPE.svg" />
+            <Faculty>
+              ภาควิชาวิศวกรรมคอมพิวเตอร์
+              <Department>
+                คณะวิศวกรรมศาสตร์มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
+              </Department>
+            </Faculty>
+          </NavbarBand>
+
+          <MenuButton> {visible ? '​​' : 'EN'}</MenuButton>
+          <Dropdown
+            overlay={NavbarMenu}
+            placement="bottomCenter"
+            onVisibleChange={showDropdown}
+            trigger={['click']}
+            overlayStyle={{ width: '100%', borderTop: '1px solid #eaeaea' }}>
+            <MenuButton>{visible ? <CloseOutlined /> : <MenuOutlined />}</MenuButton>
+          </Dropdown>
+        </Li>
       </Nav>
     </>
   );
