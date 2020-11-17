f78d8e045c9869350ad5dcb686df9dfd4bbeae03
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2020-11-17 16:02:54 +0700

    Fix navbar

--- frontend/src/modules/root/components/Navbar/components/NavbarMenu/styled.ts
+++ frontend/src/modules/root/components/Navbar/components/NavbarMenu/styled.ts
@@ -2,16 +2,24 @@ import styled from 'styled-components';
 import fontFamily from 'styles/typography/fontFamily';
 
 export const Menu = styled.div`
-  width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
+  flex-wrap: wrap;
   font-family: ${fontFamily.Default};
+  border-top: 1px solid #eaeaea;
+  background: pink;
+  margin-top: -5px;
+  padding-top: 45px;
+  padding-bottom: 40px;
 `;
 
 export const Group = styled.ul`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  width: 200px;
   list-style: none;
-  padding-top: 40px;
-  padding-bottom: 40px;
 `;
 
 export const Item = styled.li`
