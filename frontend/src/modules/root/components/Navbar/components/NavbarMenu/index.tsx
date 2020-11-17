be51f462d1e36e5d98f8786099dffb94513f89fb
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2020-11-17 23:01:51 +0700

    Fix navbar

--- frontend/src/modules/root/components/Navbar/components/NavbarMenu/index.tsx
+++ frontend/src/modules/root/components/Navbar/components/NavbarMenu/index.tsx
@@ -1,24 +1,29 @@
+import Container from 'common/components/Container';
 import NAVBAR_CONSTANT from 'modules/root/components/Navbar/constants';
 import React from 'react';
-import { Group, Item, ItemLink, Menu } from './styled';
-
+import { BorderTop, Group, Item, ItemLink, Menu } from './styled';
 const NavbarMenu = (
-  <Menu>
-    {NAVBAR_CONSTANT.map(({ key, label, subMenu }) => {
-      return (
-        <Group key={key}>
-          <Item>{label}</Item>
-          {subMenu?.map(({ key, label, link }) => {
-            return (
-              <Item key={key}>
-                <ItemLink href={link}>{label}</ItemLink>
-              </Item>
-            );
-          })}
-        </Group>
-      );
-    })}
-  </Menu>
+  <>
+    <BorderTop />
+    <Container>
+      <Menu>
+        {NAVBAR_CONSTANT.map(({ key, label, subMenu }) => {
+          return (
+            <Group key={key}>
+              <Item>{label}</Item>
+              {subMenu?.map(({ key, label, link }) => {
+                return (
+                  <Item key={key}>
+                    <ItemLink href={link}>{label}</ItemLink>
+                  </Item>
+                );
+              })}
+            </Group>
+          );
+        })}
+      </Menu>
+    </Container>
+  </>
 );
 
 export default NavbarMenu;
--- frontend/src/modules/root/page/Home/styled.ts
+++ frontend/src/modules/root/page/Home/styled.ts
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  padding: 5rem 0;
+  min-height:100vh
+  width:100%
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Title = styled.h1`
   margin: 0;
