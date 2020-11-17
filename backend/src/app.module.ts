69b03435073bf496728cc59459fe470fdb891bed
Krittaphon Raksakul <krittaphon.official@gmail.com>
2020-11-18 00:26:55 +0700

    Add database module to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
+import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule,DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
