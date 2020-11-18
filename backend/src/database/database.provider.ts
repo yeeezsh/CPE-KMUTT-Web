49309f0220156ca24d932adb4050d8a41975ee01
Krittaphon Raksakul <krittaphon.official@gmail.com>
2020-11-18 16:51:57 +0700

    Update format code

--- backend/src/database/database.provider.ts
+++ backend/src/database/database.provider.ts
@@ -1,25 +1,24 @@
-import { Provider, Logger } from '@nestjs/common';
+import { Provider } from '@nestjs/common';
 import * as mongoose from 'mongoose';
+import { ConfigurationInterface } from '../config/config.interface';
 import { DATABASE_CONNECTION } from './constants/database.constant';
-import {ConfigurationInterface} from '../config/config.interface'
 
 export const databaseProviders: Provider[] = [
   {
-    inject:['APP_CONFIG'],
+    inject: ['APP_CONFIG'],
     provide: DATABASE_CONNECTION,
-    useFactory: async (AppConfig:ConfigurationInterface): Promise<typeof mongoose> => {
-      return await mongoose.connect(
-        AppConfig.database.connection,
-        {
-          user: AppConfig.database.username,
-          pass: AppConfig.database.password,
-          authSource: AppConfig.database.authSource,
-          useCreateIndex: true,
-          useNewUrlParser: true,
-          useFindAndModify: false,
-          useUnifiedTopology: true,
-        },
-      )
+    useFactory: async (
+      AppConfig: ConfigurationInterface,
+    ): Promise<typeof mongoose> => {
+      return await mongoose.connect(AppConfig.database.connection, {
+        user: AppConfig.database.username,
+        pass: AppConfig.database.password,
+        authSource: AppConfig.database.authSource,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      });
     },
   },
 ];
