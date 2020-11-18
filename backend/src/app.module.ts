631963d5b7ccad6d73b940e8157813b3e365e4c1
Krittaphon Raksakul <krittaphon.official@gmail.com>
2020-11-18 21:55:52 +0700

    Update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
-import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  imports: [ConfigModule,DatabaseModule],
+  imports: [ConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
