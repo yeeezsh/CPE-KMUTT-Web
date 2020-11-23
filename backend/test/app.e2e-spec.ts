090c17344bb5d9938679d7eb26f8cd5789813b54
Krittaphon Raksakul <42891316+kkerberoz@users.noreply.github.com>
2020-11-23 09:41:31 +0700

    [CKW-11] Database Module (#9)
    * Fix bad tsconfig
    
    * Add mongoose
    
    * Add database module
    
    * Fix eslint error
    
    * Update docker-compose
    
    * Add database module to app
    
    * Update db password in config
    
    * Update connection env with config module
    
    * Update eslint
    
    * Fix eslint
    
    * Fix bad tsconfig
    
    * Update format code
    
    * Update
    
    * Run format
    
    * Remove unneccessary await
    
    * Initiate db only once
    
    * Format
    
    * Update auto import setting
    
    * ¯\_(ツ)_/¯
    
    * Refactor
    
    * Implement APP_CONFIG constant
    
    Co-authored-by: yee2542 <yee2542gtl@gmail.com>
    Co-authored-by: yee2542 <korbboon.thuswongsa@agoda.com>

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,15 +1,34 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as mongoose from 'mongoose';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { DATABASE_CONNECTION } from '../src/database/constants/database.constant';
+import { mockDatabaseFactory, replSet } from '../src/utils/database.factory';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let mockDB: typeof mongoose;
+
+  beforeAll(async () => {
+    mockDB = await mockDatabaseFactory();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await mongoose.disconnect();
+    await replSet.stop();
+  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(DATABASE_CONNECTION)
+      .useFactory({
+        factory: () => mockDB,
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
