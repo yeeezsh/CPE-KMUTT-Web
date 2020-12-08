b372be9ef7bffaf40c61ce72b4ad26f511c869d3
Krittaphon Raksakul <krittaphon.official@gmail.com>
2020-12-08 23:23:58 +0700

    Fix path for e2e test

--- backend/src/file/file.module.ts
+++ backend/src/file/file.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
-import { ConfigModule } from 'src/config/config.module';
-import { DatabaseModule } from 'src/database/database.module';
-import { databaseProviders } from 'src/database/database.provider';
+import { ConfigModule } from './../config/config.module';
+import { DatabaseModule } from './../database/database.module';
+import { databaseProviders } from './../database/database.provider';
 import { GridFsMulterConfigService } from './file.config';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
