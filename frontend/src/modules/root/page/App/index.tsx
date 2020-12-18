0d4683b23c89c6969ca49754e85649b0a80a1b1d
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2020-12-18 11:34:45 +0700

    Add keen-slider css

--- frontend/src/modules/root/page/App/index.tsx
+++ frontend/src/modules/root/page/App/index.tsx
@@ -1,4 +1,5 @@
 import 'antd/dist/antd.css';
+import 'keen-slider/keen-slider.min.css';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
