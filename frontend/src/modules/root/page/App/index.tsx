e914213b4c8417c564e596a3f1ec9aaecf2ddb53
nitpum <nitpum@outlook.com>
2021-01-04 13:12:32 +0700

    feat: Add global style

--- frontend/src/modules/root/page/App/index.tsx
+++ frontend/src/modules/root/page/App/index.tsx
@@ -1,8 +1,15 @@
 import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app';
 
+import { GlobalStyle } from './styled';
+
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
