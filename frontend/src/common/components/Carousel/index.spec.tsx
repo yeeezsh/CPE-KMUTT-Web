85ca23ba6050f50039829e4ca3f77f018dab1722
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-01-11 10:34:29 +0700

    Add test

---
+++ frontend/src/common/components/Carousel/index.spec.tsx
@@ -0,0 +1,30 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import Carousel from './index';
+
+describe('Carousel Tests', () => {
+  const props = {
+    item: [
+      {
+        id: '1',
+        picture: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+        heading: 'Test',
+        caption: 'This is caption',
+        link: '#',
+      },
+      {
+        id: '2',
+        picture: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+        heading: 'Test++',
+        caption: 'Caption is here',
+        link: '#',
+      },
+    ],
+    fullText: false,
+    arrows: true,
+  };
+  it('Carousel component should be defined', () => {
+    const wrap = shallow(<Carousel variant="Default" {...props} />);
+    expect(wrap.exists()).toBe(true);
+  });
+});
