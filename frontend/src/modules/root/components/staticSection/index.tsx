7546b65beeb2f386317c173ff37ca9cb57930b6f
UnkAtreus <nom_noon00548@hotmail.com>
2021-01-16 22:19:25 +0700

    Add StaticSection Floder

---
+++ frontend/src/modules/root/components/staticSection/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StaticSectionStyle } from './styled';
+
+const WhatNewSection: React.FC = () => {
+  return <StaticSectionStyle></StaticSectionStyle>;
+};
+
+export default WhatNewSection;
---
+++ frontend/src/modules/root/components/staticSection/styled.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StaticSectionStyle = styled.div`
+  font-family: 'Kanit', sans-serif;
+`;
