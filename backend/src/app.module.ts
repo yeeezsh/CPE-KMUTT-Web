e419e488049f08a616e161deef277d20afad4541
Krittaphon Raksakul <krittaphon.official@gmail.com>
2021-01-17 15:48:48 +0700

    update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { FileModule } from './file/file.module';
+import { AppResolver } from './app.resolver';
 @Module({
-  imports: [ConfigModule, DatabaseModule, FileModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    FileModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
