e82194e379cb7d9589661a0a75f0af032dfe4675
UnkAtreus <nom_noon00548@hotmail.com>
2021-01-19 22:26:45 +0700

    del static floder

--- frontend/src/modules/root/components/staticSection/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StaticSectionStyle } from './styled';
-
-const WhatNewSection: React.FC = () => {
-  return <StaticSectionStyle></StaticSectionStyle>;
-};
-
-export default WhatNewSection;
--- frontend/src/modules/root/components/staticSection/styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const StaticSectionStyle = styled.div`
-  font-family: 'Kanit', sans-serif;
-`;
