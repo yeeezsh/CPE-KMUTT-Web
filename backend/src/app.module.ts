c2d74df6a9a845d96c163ed0377882c81f3b2b90
Krittaphon Raksakul <42891316+kkerberoz@users.noreply.github.com>
2021-01-28 15:26:30 +0700

    Graphql setup (#16)
    * Fix eslint
    
    * Add gridfs and multer
    
    * Create upload file module
    
    * Fix path for e2e test
    
    * Add custom type and change injection string
    
    * add mock file for testing
    
    * add unit test and e2e
    
    * add forceExit command for jest
    
    * fix testing
    
    * Add file-error exception
    
    * move file e2e test  from app.e2e to file.e2e
    
    * install graphql
    
    * add app resolver and testing
    
    * update app module
    
    * delete file module
    
    * delete no need graphql config
    
    * change mongoose version
    
    Co-authored-by: yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { AppResolver } from './app.resolver';
 import { FileModule } from './file/file.module';
 @Module({
-  imports: [ConfigModule, DatabaseModule, FileModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+    }),
+    FileModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
