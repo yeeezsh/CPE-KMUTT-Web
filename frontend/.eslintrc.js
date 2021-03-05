26ec20bf157f97f8211d863fc46d99c0113833ad
nitpum <nitpum@outlook.com>
2021-03-06 00:26:52 +0700

    chore: Enforce import order

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   env: {
@@ -20,26 +20,66 @@ module.exports = {
     jest: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended", // Make sure this is always the last element in the array.
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
   ],
+  plugins: ['import'],
   rules: {
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "simple-import-sort/sort": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/sort': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
       {
-        components: ["Link"],
-        specialLink: ["hrefLeft", "hrefRight"],
-        aspects: ["invalidHref", "preferButton"],
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          // {
+          //   pattern: 'next',
+          //   group: 'external',
+          //   position: 'after',
+          // },
+          // {
+          //   pattern: 'next/**',
+          //   group: 'external',
+          //   position: 'after',
+          // },
+          {
+            pattern: 'common/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'modules/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ['react', 'next'],
       },
     ],
   },
