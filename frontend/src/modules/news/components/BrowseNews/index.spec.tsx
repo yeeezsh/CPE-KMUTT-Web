6518532986454d86d3949c456f9a86ce127f7dba
yee2542 <yee2542gtl@gmail.com>
2021-04-20 22:46:53 +0700

    add more case on unit test

--- frontend/src/modules/news/components/BrowseNews/index.spec.tsx
+++ frontend/src/modules/news/components/BrowseNews/index.spec.tsx
@@ -1,7 +1,13 @@
+import Container from 'common/components/Container';
 import { shallow } from 'enzyme';
 import React from 'react';
 import BrowseNews from '.';
-import { BrowseNewsStyle } from './styled';
+import {
+  BrowseNewsRow,
+  BrowseNewsSeeMore,
+  BrowseNewsStyle,
+  BrowsNewsHeader,
+} from './styled';
 
 describe('BrowseNews Compnent', () => {
   it('BrowseNews Compnent should be defined', () => {
@@ -12,5 +18,9 @@ describe('BrowseNews Compnent', () => {
   it('BrowseNews Compnent should import these components', () => {
     const wrap = shallow(<BrowseNews />);
     expect(wrap.find(BrowseNewsStyle).exists()).toBe(true);
+    expect(wrap.find(Container).exists()).toBe(true);
+    expect(wrap.find(BrowsNewsHeader).exists()).toBe(true);
+    expect(wrap.find(BrowseNewsRow).exists()).toBe(true);
+    expect(wrap.find(BrowseNewsSeeMore).exists()).toBe(true);
   });
 });
