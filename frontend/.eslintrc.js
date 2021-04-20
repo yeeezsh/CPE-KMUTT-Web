bfdd15553382eabd8d3d97aec581e81cc8b18d31
nitpum <nitpum@outlook.com>
2021-04-20 21:43:27 +0700

    chore: Setup tests

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     'simple-import-sort/sort': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
