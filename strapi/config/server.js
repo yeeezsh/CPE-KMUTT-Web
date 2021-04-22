a41c67f9f1b1c7d2a6697ddd872040ea6a27df55
yee2542 <yee2542gtl@gmail.com>
2021-04-22 21:17:53 +0700

    reset strapi

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f545f5295110b55cb97da69a87311ee0'),
-    },
-  },
-});
--- strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd9bcce6f-c1fd-4bcd-bac6-edfadb55cba3'
-};
