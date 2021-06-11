f323055b38ce1b1fad3e9d8334e47a247c753210
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-06-11 13:43:36 +0700

    Revert "Merge branch 'main' of https://github.com/yee2542/CPE-KMUTT-Web into CKW6-pick"
    This reverts commit 5c615811d13474b8b0d464554b311c98a555d37f.

--- frontend/src/modules/home/page/Home/index.tsx
+++ frontend/src/modules/home/page/Home/index.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 
 import Head from 'next/head';
 
-import Carousel from 'common/components/Carousel';
-import {
-  CAROUSEL_DEFAULT_CONSTANT,
-  CAROUSEL_SLIDER_CONSTANT,
-} from 'common/components/Carousel/constant';
-
 import { Main } from './styled';
 
 const Home: React.FC = () => {
@@ -17,10 +11,7 @@ const Home: React.FC = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main>
-        <Carousel variant="Default" item={CAROUSEL_DEFAULT_CONSTANT} />
-        <Carousel variant="Slider" item={CAROUSEL_SLIDER_CONSTANT} />
-      </Main>
+      <Main></Main>
     </div>
   );
 };
