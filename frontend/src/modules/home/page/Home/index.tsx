d7dc0241d74e06a8cfee0bd80178b1d48eb8a485
yee2542 <yee2542gtl@gmail.com>
2021-06-21 21:10:52 +0700

    bring home back

--- frontend/src/modules/home/page/Home/index.tsx
+++ frontend/src/modules/home/page/Home/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 import Head from 'next/head';
+import { HiOutlineArrowRight } from 'react-icons/hi';
 
+import { Btn } from 'common/components/Button/styled';
+
+import Footer from 'modules/home/components/Footer';
+import StaticSection from 'modules/home/components/StaticSection';
+import { ButtonStyled } from 'modules/home/components/StaticSection/styled';
+import WhatNewSection from 'modules/home/components/WhatNewSection';
 import Navbar from 'modules/root/components/Navbar';
 
 import { Main } from './styled';
@@ -9,12 +16,54 @@ import { Main } from './styled';
 const Home: React.FC = () => {
   return (
     <div>
+      <Navbar />
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main>
-        <Navbar />
+        <WhatNewSection />
+        <StaticSection
+          id="1"
+          background="/images/static_bg_curriculum.png"
+          header="ที่วิศวกรรมคอมพิวเตอร์ มจธ. เรามีหลักสูตรหลากหลาย"
+          content="เซ็นเตอร์เวิร์คฟลุทแอดมิสชันออร์แกน ใช้งานรูบิค แซมบ้าเมาท์
+          แอดมิชชั่นคัตเอาต์โครนาโอ้ยเซ็นเซอร์ ผู้นำเมจิควิทย์ บลอนด์แทคติคไอซียูเยลลี่">
+          <ButtonStyled>
+            <Btn $color="primary">
+              สำรวจหลักสูตรทั้งหมด
+              <HiOutlineArrowRight className="Icon" style={{ marginLeft: '16px' }} />
+            </Btn>
+          </ButtonStyled>
+          <ButtonStyled>
+            <Btn $color="white">
+              สมัครเข้าเรียน
+              <HiOutlineArrowRight
+                className="Icon"
+                style={{ marginLeft: '16px' }}
+                size="16px"
+              />
+            </Btn>
+          </ButtonStyled>
+        </StaticSection>
+
+        <StaticSection
+          id="2"
+          background="/images/static_bg_gallery.png"
+          header="แกลเลอรี่รูปภาพ"
+          content="โดยมีอาจารย์ผู้สอนเป็นผู้ให้คำปรึกษา นักศึกษาต้องทำงานเป็นกลุ่ม และมีการนำเสนอผลงาน ของตนเองให้เพื่อนๆ ร่วมกันวิเคราะห์และวิจารณ์อย่างมีเหตุผล">
+          <ButtonStyled>
+            <Btn $color="white">
+              สำรวจแกลเลอรี่
+              <HiOutlineArrowRight
+                className="Icon"
+                style={{ marginLeft: '16px' }}
+                size="16px"
+              />
+            </Btn>
+          </ButtonStyled>
+        </StaticSection>
+        <Footer key="1"></Footer>
       </Main>
     </div>
   );
