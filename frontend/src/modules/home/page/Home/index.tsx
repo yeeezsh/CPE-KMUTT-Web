b412bd6767ba562df78dbbfacfecffd511d480bc
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-06-21 22:12:09 +0700

    CKW9-pick (#37)
    * Merge branch 'main' of https://github.com/yee2542/CPE-KMUTT-Web into CKW6-pick
    
    * Revert "Merge branch 'main' of https://github.com/yee2542/CPE-KMUTT-Web into CKW6-pick"
    
    This reverts commit 5c615811d13474b8b0d464554b311c98a555d37f.
    
    * Revert "Revert "Merge branch 'main' of https://github.com/yee2542/CPE-KMUTT-Web into CKW6-pick""
    
    This reverts commit f323055b38ce1b1fad3e9d8334e47a247c753210.
    
    * fix codesmell
    
    * fix codesmell
    
    * fix codesmell
    
    * fix smell
    
    * Fix style
    
    Co-authored-by: yee2542 <yee2542gtl@gmail.com>
    Co-authored-by: yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>

--- frontend/src/modules/home/page/Home/index.tsx
+++ frontend/src/modules/home/page/Home/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import Head from 'next/head';
 
+import Carousel from 'common/components/Carousel';
+import {
+  CAROUSEL_DEFAULT_CONSTANT,
+  CAROUSEL_SLIDER_CONSTANT,
+} from 'common/components/Carousel/constant';
+
 import { Main } from './styled';
 
 const Home: React.FC = () => {
@@ -11,7 +17,10 @@ const Home: React.FC = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main></Main>
+      <Main>
+        <Carousel variant="Default" item={CAROUSEL_DEFAULT_CONSTANT} />
+        <Carousel variant="Slider" item={CAROUSEL_SLIDER_CONSTANT} />
+      </Main>
     </div>
   );
 };
