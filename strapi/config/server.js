dbfd74f8011cdbc67dece81ccbc457dde97bb166
yee2542 <yee2542gtl@gmail.com>
2021-09-06 00:55:11 +0700

    fix: :wrench: allow internal cors domain

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,9 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ad43bc17347d4a944ca16e9d9464f6ba'),
+      secret: env("ADMIN_JWT_SECRET", "ad43bc17347d4a944ca16e9d9464f6ba"),
+    },
+  },
+  settings: {
+    cors: {
+      origin: [
+        "http://localhost",
+        "http://localhost:5000",
+        "http://localhost:1337",
+      ],
     },
   },
 });
