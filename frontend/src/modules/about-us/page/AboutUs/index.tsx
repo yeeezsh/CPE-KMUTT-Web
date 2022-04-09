01bc854a800c01477c5613d177d2d16237765cf9
Kittipat Dechkul <atreus@Kittipats-MacBook-Air.local>
2022-04-09 14:39:03 +0700

    feat: NavitionMap

--- frontend/src/modules/about-us/page/AboutUs/index.tsx
+++ frontend/src/modules/about-us/page/AboutUs/index.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Head from 'next/head';
 
 import Header from 'common/components/Header';
+import NavigationMap from 'common/components/NavigationMap';
 
 import Footer from 'modules/home/components/Footer';
 import Navbar from 'modules/root/components/Navbar';
 
 const AboutUs: React.FC = () => {
+  const [aboutTitle, setAboutTitle] = useState(`เกี่ยวกับภาควิชาฯ`);
   return (
     <>
       <Head>
-        <title>CPE KMUTT</title>
+        <title>ABOUT US | CPE KMUTT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <Header title="เกี่ยวกับเรา" backgroundImage="/images/about-us-banner.png" />
+      <NavigationMap
+        breadcrumb={[{ title: `เกี่ยวกับเรา`, url: `/` }, { title: aboutTitle }]}
+      />
       <Footer key="1" />
     </>
   );
