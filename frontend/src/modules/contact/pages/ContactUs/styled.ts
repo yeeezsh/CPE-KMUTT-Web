a252a35f30f0ea4cbd1e900af63077701638da0c
nitpum <nitpum@outlook.com>
2022-04-11 00:35:21 +0700

    feat: update contact section

--- frontend/src/modules/contact/pages/ContactUs/styled.ts
+++ frontend/src/modules/contact/pages/ContactUs/styled.ts
@@ -9,6 +9,11 @@ export const StyledRow = styled(Row)`
   width: 100%;
 `;
 
+export const ContactSection = styled.section`
+  margin-top: 40px;
+  margin-bottom: 50px;
+`;
+
 export const Address = styled.div`
   color: ${colors.GRAY_1};
   margin-bottom: ${SPACE_FOR_CONTACT_DETAIL};
