25cbd47a4af67e9f013610205213941aa56d423e
yee2542 <yee2542gtl@gmail.com>
2022-04-10 23:46:54 +0700

    refactor: :recycle: useNewsContentParser

---
+++ frontend/src/modules/news/hooks/useNewsContentParser.tsx
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs';
+
+import {
+  GetNewsByIdQuery,
+  NewsAndAnnouncementDynamicContentDynamicZone,
+} from 'common/generated/generated-types';
+import { joinImageStrapi } from 'common/utils/join';
+
+import { NewsContentContainer } from 'modules/news/components/NewsContent/styled';
+
+export type UseNewsContentParser = {
+  header: string | undefined;
+  title: string | undefined;
+  thumbnail: string | undefined;
+  postDate: string;
+  contents: JSX.Element[] | undefined;
+};
+
+export const useNewsContentParser = (data?: GetNewsByIdQuery): UseNewsContentParser => {
+  function mapContents(
+    dynamic_element?: NewsAndAnnouncementDynamicContentDynamicZone,
+  ): JSX.Element {
+    if (dynamic_element?.__typename === 'ComponentContentSectionsTextContent')
+      return (
+        <NewsContentContainer
+          dangerouslySetInnerHTML={{ __html: dynamic_element.body as string }}
+        />
+      );
+
+    return <></>;
+  }
+
+  const header = data?.newsAndAnnouncement?.header;
+  const title = data?.newsAndAnnouncement?.header;
+  const thumbnail = joinImageStrapi(
+    data?.newsAndAnnouncement?.canvas_preview?.url as string,
+  );
+  const postDate = dayjs(data?.newsAndAnnouncement?.createdAt).format(
+    'dddd, DD MMMM YYYY',
+  );
+  const contents = data?.newsAndAnnouncement?.dynamic_content?.map((e) =>
+    mapContents(e as NewsAndAnnouncementDynamicContentDynamicZone),
+  );
+
+  return { header, title, thumbnail, postDate, contents };
+};
--- frontend/src/modules/news/page/NewsContentPage/index.tsx
+++ frontend/src/modules/news/page/NewsContentPage/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 
-import dayjs from 'dayjs';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 
 import CommonLayout from 'common/components/Layouts/CommonLayout';
-import {
-  GetNewsByIdQuery,
-  NewsAndAnnouncementDynamicContentDynamicZone,
-} from 'common/generated/generated-types';
-import { joinImageStrapi } from 'common/utils/join';
+import { GetNewsByIdQuery } from 'common/generated/generated-types';
 
 import NewsContent from 'modules/news/components/NewsContent';
-import { NEWS_CONTENT } from 'modules/news/components/NewsContent/constant';
-import { NewsContentContainer } from 'modules/news/components/NewsContent/styled';
+import { useNewsContentParser } from 'modules/news/hooks/useNewsContentParser';
 
 const MOCK_NAVIGATE = [
   {
@@ -35,48 +28,21 @@ const MOCK_NAVIGATE = [
 ];
 
 const NewsContentPage: React.FC<{ data?: GetNewsByIdQuery }> = ({ data }) => {
-  // TODO: move this to hooks
-
-  // tags
-
   // NewsContent
-  function mapContents(
-    dynamic_element?: NewsAndAnnouncementDynamicContentDynamicZone,
-  ): JSX.Element {
-    if (dynamic_element?.__typename === 'ComponentContentSectionsTextContent')
-      return (
-        <NewsContentContainer
-          dangerouslySetInnerHTML={{ __html: dynamic_element.body as string }}
-        />
-      );
-
-    return <></>;
-  }
-
-  const header = data?.newsAndAnnouncement?.header;
-  const title = data?.newsAndAnnouncement?.header;
-  const thumbnail = data?.newsAndAnnouncement?.canvas_preview?.url;
-  const postDate = dayjs(data?.newsAndAnnouncement?.createdAt).format(
-    'dddd, DD MMMM YYYY',
-  );
-  const contents = data?.newsAndAnnouncement?.dynamic_content?.map((e) =>
-    mapContents(e as NewsAndAnnouncementDynamicContentDynamicZone),
-  );
+  const { header, title, postDate, thumbnail, contents } = useNewsContentParser(data);
 
   return (
     <>
       <Head>
-        <title>
-          {/* //TODO: join heading */}
-          {header}
-        </title>
+        <title>{header}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* //TODO: remove this mock tag*/}
       <CommonLayout header="ข่าวสารและประกาศ" navigate={MOCK_NAVIGATE}>
         <NewsContent
           title={title}
           postDate={postDate}
-          tumbnail={joinImageStrapi(thumbnail as string)}
+          tumbnail={thumbnail}
           contents={contents}
         />
       </CommonLayout>
