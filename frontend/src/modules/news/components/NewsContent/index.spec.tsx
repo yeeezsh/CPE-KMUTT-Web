5ae342c7608ba26ffddce2dd9616debeca7173ef
yee2542 <yee2542gtl@gmail.com>
2022-04-11 22:44:19 +0700

    test: :white_check_mark: fix NewsContent test

--- frontend/src/modules/news/components/NewsContent/index.spec.tsx
+++ frontend/src/modules/news/components/NewsContent/index.spec.tsx
@@ -30,13 +30,32 @@ describe('NewsContent Compnent', () => {
     expect(wrap.find(NewsContentTumbnail).exists()).toBe(true);
     expect(wrap.find(Container).exists()).toBe(true);
     // expect(wrap.find(NewsContentContainer).exists()).toBe(true);
+    // expect(wrap.find(NewsContentFileConteiner).exists()).toBe(true);
+    // expect(wrap.find(NewsContentFileHeader).exists()).toBe(true);
+    // expect(wrap.find(NewsContentFileList).exists()).toBe(true);
+    // expect(wrap.find(NewsContentFileName).exists()).toBe(true);
+    // expect(wrap.find(NewsContentFileNameContainer).exists()).toBe(true);
+    expect(wrap.find(NewsContentHeader).exists()).toBe(true);
+    expect(wrap.find(NewsContentPostDate).exists()).toBe(true);
+    expect(wrap.find(NewsContentStyle).exists()).toBe(true);
+  });
+
+  it('NewsContent should show content section when have data from API', () => {
+    const wrap = shallow(
+      <NewsContent
+        tumbnail="/images/thumbnail2.png"
+        contents={[<NewsContentContainer key={'1'}>news content</NewsContentContainer>]}
+      />,
+    );
+    expect(wrap.find(NewsContentContainer).exists()).toBe(true);
+  });
+
+  it('NewsContent should show download section when have data from API', () => {
+    const wrap = shallow(<NewsContent file={[{ title: 'test', link: '/' }]} />);
     expect(wrap.find(NewsContentFileConteiner).exists()).toBe(true);
     expect(wrap.find(NewsContentFileHeader).exists()).toBe(true);
     expect(wrap.find(NewsContentFileList).exists()).toBe(true);
     expect(wrap.find(NewsContentFileName).exists()).toBe(true);
     expect(wrap.find(NewsContentFileNameContainer).exists()).toBe(true);
-    expect(wrap.find(NewsContentHeader).exists()).toBe(true);
-    expect(wrap.find(NewsContentPostDate).exists()).toBe(true);
-    expect(wrap.find(NewsContentStyle).exists()).toBe(true);
   });
 });
