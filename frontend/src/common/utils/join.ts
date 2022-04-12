4ad6402b7afee9d40390b9da895c5adeab3f2e5b
yee2542 <yee2542gtl@gmail.com>
2022-04-13 03:19:59 +0700

    refactor: :recycle: fix smell code

--- frontend/src/common/utils/join.ts
+++ frontend/src/common/utils/join.ts
@@ -1,4 +1,4 @@
 import { STRAPI_ENDPOINT } from 'common/constants';
 
-export const joinImageStrapi = (url: string) => STRAPI_ENDPOINT + url;
-export const joinFileDownloadStrapi = (url: string) => STRAPI_ENDPOINT + url;
+export const joinImageStrapi = (url: string): string => STRAPI_ENDPOINT + url;
+export const joinFileDownloadStrapi = (url: string): string => STRAPI_ENDPOINT + url;
