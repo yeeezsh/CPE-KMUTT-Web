f795e5f567f2399507ea88d4eca05fd8e219bae2
yeeezsh <yee2542gtl@gmail.com>
2022-04-12 23:44:41 +0700

    [CKW-50] WYSIWYG implement on News Content (#57)
    * feat: :sparkles: add text content
    
    * refactor: :recycle: reuse component logic
    
    * refactor: :recycle: remove duplicated dynamic comps
    
    * fix: :bug: bad schema
    
    * feat: :sparkles: add get news by id gql
    
    * feat: :sparkles: news & announcement API
    
    * refactor: :recycle: deprecate content api
    
    * chore: :wrench: update strapi api
    
    * fix: :ambulance: strapi break changes api
    
    * test: :white_check_mark: bad mock type
    
    * fix: :lipstick: bad description parsed
    
    since we use dynamic content as HTML, parsing text need to remove html tag
    
    * feat: :sparkles: add seo link
    
    * refactor: :recycle: remove uneccessary default value
    
    * build: :package: add dayjs
    
    * build: :wrench: allow synthesis import
    
    * fix: :bug: do not allow space in SEO links
    
    * feat: :sparkles: parse news from backend api
    
    * refactor: :recycle: useNewsContentParser
    
    * refactor: :recycle: convention file name
    
    * test: :white_check_mark: add useNewsContentParser test
    
    * feat: :sparkles: bind content news to WYSIWYG
    
    * fix: :ambulance: bad dom render
    
    * test: :white_check_mark: fix test
    
    * feat: :sparkles: seo_link for tags
    
    * feat: :sparkles: allow query news by id
    
    * feat: :sparkles: implement tags from API
    
    * feat: :sparkles: add files download
    
    * test: :white_check_mark: fix NewsContent test
    
    * chore: :wrench: update generated types
    
    * refactor: :recycle: remove unused code
    
    * fix: :zap: bad key rendering
    
    * test: :white_check_mark: fix useNewsContentParser test
    
    * refactor: :recycle: remove magic code
    
    * feat: :sparkles: news connection
    
    * refactor: :recycle: rename constant
    
    * refactor: :recycle: remove smell code
    
    * wtf: github ci
    
    * test: :white_check_mark: add connection news parser test
    
    * Revert "wtf: github ci"
    
    This reverts commit c0272a538b48dec1d2821a21b8e574e7389bbb74.
    
    * fuck you ci
    
    * Revert "fuck you ci"
    
    This reverts commit dd6b305670a8edc469201655fc72bee30a19c71a.
    
    * ci: :fire: conflict timezone for testing
    
    * Revert "ci: :fire: conflict timezone for testing"
    
    This reverts commit 78225fb3d4140e13b1afeb9a641292f6f763bcad.
    
    * test: :white_check_mark: fix timbomb test in useNewsContentParser
    
    * docs: :memo: add memo
    
    * fix: :ambulance: bad link render
    
    * fix: :bug: too short description
    
    * fix: :ambulance: bad __typename reference

---
+++ frontend/src/common/constants/links.ts
@@ -0,0 +1,4 @@
+export const STATIC_HOME_LINK = '/';
+export const STATIC_NEWS_LINK = '/news';
+export const STATIC_NEWS_CATEGORY_LINK = '/news/category';
+export const STATIC_NEWS_ID_LINK = '/news/[id]';
--- frontend/src/common/utils/join.ts
+++ frontend/src/common/utils/join.ts
@@ -1,3 +1,4 @@
 import { STRAPI_ENDPOINT } from 'common/constants';
 
 export const joinImageStrapi = (url: string) => STRAPI_ENDPOINT + url;
+export const joinFileDownloadStrapi = (url: string) => STRAPI_ENDPOINT + url;
