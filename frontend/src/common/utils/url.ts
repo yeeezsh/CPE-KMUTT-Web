d8503445df93f407ae5f10207fbb38a00a10479d
yee2542 <yee2542gtl@gmail.com>
2022-04-27 01:08:50 +0700

    fix: :bug: bad build url
    cannot use new URL() idk why

--- frontend/src/common/utils/url.ts
+++
@@ -1,7 +0,0 @@
-import { URL } from 'url';
-
-import { STRAPI_ENDPOINT } from 'common/constants';
-
-export const ImageStrapiUrl = (url: string): string => new URL(url, STRAPI_ENDPOINT).href;
-export const FileDownloadStrapiUrl = (url: string): string =>
-  new URL(url, STRAPI_ENDPOINT).href;
