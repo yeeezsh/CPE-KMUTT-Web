d30986a82bb8e8bc7d6751b83223a1a64cd49ab7
yee2542 <yee2542gtl@gmail.com>
2022-06-23 17:25:59 +0700

    feat: :sparkles: initialize gtm

--- frontend/src/modules/root/pages/App/index.tsx
+++ frontend/src/modules/root/pages/App/index.tsx
@@ -3,12 +3,19 @@ import React from 'react';
 
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import TagManager from 'react-gtm-module';
 import { Provider } from 'react-redux';
 
 import { client } from 'common/services/client';
 import { store } from 'common/stores';
 
 import { GlobalStyle } from './styled';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PF6LN2L',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
