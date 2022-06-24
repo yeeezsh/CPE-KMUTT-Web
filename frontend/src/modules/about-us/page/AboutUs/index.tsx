e833034b40bb73a5d90556ac02142f889d5498cb
yee2542 <yee2542gtl@gmail.com>
2022-06-25 01:32:02 +0700

    refactor: :recycle: use common layout

--- frontend/src/modules/about-us/page/AboutUs/index.tsx
+++ frontend/src/modules/about-us/page/AboutUs/index.tsx
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from 'react';
 
 import Head from 'next/head';
 
-import Container from 'common/components/Container';
-import Header from 'common/components/Header';
-import { FaqWithBanner } from 'common/components/Layouts/CommonLayout';
-import NavigationMap from 'common/components/NavigationMap';
+import CommonLayout from 'common/components/Layouts/CommonLayout';
+import { STATIC_ABOUT_US_LINK, STATIC_HOME_LINK } from 'common/constants/links';
 
 import ContentSection from 'modules/about-us/components/ContentSection';
 import SidebarSection from 'modules/about-us/components/SidebarSection';
 import { sidebarItems } from 'modules/about-us/components/SidebarSection/constants';
 import { AboutUsContainer } from 'modules/about-us/page/AboutUs/styled';
-import Footer from 'modules/home/components/Footer';
-import Navbar from 'modules/root/components/Navbar';
 
 const AboutUs: React.FC<{ id: string }> = ({ id }) => {
   const [aboutTitle, setAboutTitle] = useState(`เกี่ยวกับภาควิชาฯ`);
+
+  const DEFAULT_NAVIGATE = [
+    {
+      title: 'หน้าแรก',
+      link: STATIC_HOME_LINK,
+    },
+    {
+      title: aboutTitle || 'เกี่ยวกับเรา',
+      link: STATIC_ABOUT_US_LINK,
+    },
+  ];
 
   useEffect(() => {
     const title = sidebarItems
@@ -32,17 +39,15 @@ const AboutUs: React.FC<{ id: string }> = ({ id }) => {
         <title>ABOUT US | CPE KMUTT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <Header title="เกี่ยวกับเรา" backgroundImage="/images/about-us-banner.png" />
-      <NavigationMap
-        breadcrumb={[{ title: `เกี่ยวกับเรา`, url: `/` }, { title: aboutTitle }]}
-      />
-      <AboutUsContainer>
-        <SidebarSection activeKey={id} />
-        <ContentSection />
-      </AboutUsContainer>
-      <FaqWithBanner />
-      <Footer key="1" />
+
+      <CommonLayout header="เกี่ยวกับเรา" navigate={DEFAULT_NAVIGATE}>
+        {/* TODO://make coment layout accept bg props */}
+        {/* <Header title="เกี่ยวกับเรา" backgroundImage="/images/about-us-banner.png" /> */}
+        <AboutUsContainer>
+          <SidebarSection activeKey={id} />
+          <ContentSection />
+        </AboutUsContainer>
+      </CommonLayout>
     </>
   );
 };
