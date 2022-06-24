aea6e4a6e7c7b954ab4f379c572a9f0e19e49f33
yee2542 <yee2542gtl@gmail.com>
2022-06-25 01:24:13 +0700

    refactor: :recycle: remove usued code

--- frontend/src/pages/about-us/index.tsx
+++ frontend/src/pages/about-us/index.tsx
@@ -1,13 +1,3 @@
-import { GetServerSideProps } from 'next';
-
 import AboutUs from 'modules/about-us/page/AboutUs';
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {
-      id: `history`,
-    },
-  };
-};
 
 export default AboutUs;
