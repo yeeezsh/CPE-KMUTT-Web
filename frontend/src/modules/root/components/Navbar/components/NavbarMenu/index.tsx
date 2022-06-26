fbd2927ebe0b9f768f78be527269a661f195720b
yee2542 <yee2542gtl@gmail.com>
2022-06-26 15:15:08 +0700

    feat: :sparkles: add contact-us link to navbar

--- frontend/src/modules/root/components/Navbar/components/NavbarMenu/index.tsx
+++ frontend/src/modules/root/components/Navbar/components/NavbarMenu/index.tsx
@@ -15,37 +15,39 @@ import {
   SubMenuItem,
 } from './styled';
 
-const NavbarMenu: React.FC = () => (
-  <MenuWrap>
-    <Container>
-      <Menu>
-        {NAVBAR_MENU.map(({ key, label, subMenu, picture }) => {
-          return (
-            <Group key={key}>
-              {picture ? (
-                <Image img={picture}>
-                  <Mask />
-                </Image>
-              ) : null}
-              <Item>{label}</Item>
-              {subMenu?.map(({ key: itemKey, label: ItemLabel, link }) => {
-                return (
-                  <ItemLink key={key + '-submenu-' + itemKey} href={link}>
-                    {label}
-                  </ItemLink>
-                );
-              })}
-            </Group>
-          );
-        })}
-      </Menu>
-      <SubMenu>
-        {NAVBAR_SUB_MENU.map(({ key, label }) => {
-          return <SubMenuItem key={key}>{label}</SubMenuItem>;
-        })}
-      </SubMenu>
-    </Container>
-  </MenuWrap>
-);
+const NavbarMenu: React.FC = () => {
+  return (
+    <MenuWrap>
+      <Container>
+        <Menu>
+          {NAVBAR_MENU.map(({ key, label, subMenu, picture }) => {
+            return (
+              <Group key={key}>
+                {picture ? (
+                  <Image img={picture}>
+                    <Mask />
+                  </Image>
+                ) : null}
+                <Item>{label}</Item>
+                {subMenu?.map(({ key: itemKey, label: ItemLabel, link }) => {
+                  return (
+                    <ItemLink key={key + '-submenu-' + itemKey} href={link}>
+                      {label}
+                    </ItemLink>
+                  );
+                })}
+              </Group>
+            );
+          })}
+        </Menu>
+        <SubMenu>
+          {NAVBAR_SUB_MENU.map(({ key, label }) => {
+            return <SubMenuItem key={key}>{label}</SubMenuItem>;
+          })}
+        </SubMenu>
+      </Container>
+    </MenuWrap>
+  );
+};
 
 export default NavbarMenu;
