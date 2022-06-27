24e4cec8afe9b077bc2447fb91159506559fb3cb
yee2542 <yee2542gtl@gmail.com>
2022-06-28 02:37:36 +0700

    feat: :sparkles: add strapi prefix api

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: "/strapi",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ad43bc17347d4a944ca16e9d9464f6ba"),
