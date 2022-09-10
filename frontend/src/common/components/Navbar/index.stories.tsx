f541ffa117235f8faafbbac4186457454b07cbce
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-09-11 01:37:41 +0700

    fix: :bug: bad file reference

--- frontend/src/common/components/Navbar/index.stories.tsx
+++ frontend/src/common/components/Navbar/index.stories.tsx
@@ -4,8 +4,6 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 
 import Navbar from 'common/components/Navbar';
 
-import { NavBarProps } from './interface';
-
 export default {
   title: 'common/components/Navbar',
   component: Navbar,
@@ -14,8 +12,3 @@ export default {
 const Template: Story = (args) => <Navbar {...args} />;
 
 export const Default = Template.bind({});
-
-export const Visible = Template.bind({});
-Visible.args = {
-  visible: true,
-} as NavBarProps;
