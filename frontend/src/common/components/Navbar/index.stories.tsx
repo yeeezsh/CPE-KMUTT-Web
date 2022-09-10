e0cdf0323b65ac41b8b3737065eaf1d05bb177bf
nitpum <nitpum@outlook.com>
2022-09-11 01:17:53 +0700

    Fix snapshot

--- frontend/src/common/components/Navbar/index.stories.tsx
+++ frontend/src/common/components/Navbar/index.stories.tsx
@@ -4,8 +4,6 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 
 import Navbar from 'common/components/Navbar';
 
-import { NavBarProps } from './interface';
-
 export default {
   title: 'common/components/Navbar',
   component: Navbar,
@@ -18,4 +16,4 @@ export const Default = Template.bind({});
 export const Visible = Template.bind({});
 Visible.args = {
   visible: true,
-} as NavBarProps;
+};
