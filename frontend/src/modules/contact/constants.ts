1ea6d2b8c7ea7d268fb6eb04ab90c0a59c090cc7
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-09-11 02:35:13 +0700

    refactor: :recycle: move hardcoded to constant

--- frontend/src/modules/contact/constants.ts
+++ frontend/src/modules/contact/constants.ts
@@ -6,3 +6,17 @@ export const CONTACT_US_PAGE_CRUMB = {
   title: 'ติดต่อเรา',
   link: STATIC_CONTACT_US,
 };
+
+export const DEFAULT_GMAPS_URL =
+  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1630.1330306733955!2d100.49378030791785!3d13.649757914204436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2a252224fdc01%3A0xdb5f69b03ef7ef07!2sComputer%20Engineering%20Department%2C%20KMUTT!5e0!3m2!1sen!2sth!4v1649610824894!5m2!1sen!2sth';
+
+export const DEFAULT_CONTACT_PHONE = '(+66) 0 2470 9388';
+
+export const DEFAULT_CONTACT_EMAIL = 'nongyao.jam@mail.kmutt.ac.th';
+
+export const DEFAULT_CONTACT_ADDRESS = `
+ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์ 
+ชั้น 10-11, อาคารวิศววัฒนะ,
+มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี 126 ถนนประชาอุทิศ แขวงบางมด
+เขตทุ่งครุ กรุงเทพฯ 10140
+`;
