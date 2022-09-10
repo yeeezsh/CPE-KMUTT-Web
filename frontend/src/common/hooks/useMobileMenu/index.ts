1c06a1310eb05aaf971cdb4bdc05cc3f6369209d
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-09-11 00:08:09 +0700

    refactor: :recycle: implement UseMobileMenuType
    (cherry picked from commit 3b6b135bf8ac5d10b005e0c62c71b5cde306b0fe)

--- frontend/src/common/hooks/useMobileMenu/index.ts
+++ frontend/src/common/hooks/useMobileMenu/index.ts
@@ -1,8 +1,15 @@
-import { useCallback, useContext } from 'react';
+import { Dispatch, SetStateAction, useCallback, useContext } from 'react';
 
 import { NavbarContext } from 'common/contexts/navbarContext';
 
-export default function useMobileMenu() {
+export type UseMobileMenuType = {
+  isMobileMenuOpened: boolean;
+  setMobileMenuOpen: Dispatch<SetStateAction<boolean>>;
+  openMobileMenu: () => void;
+  closeMobileMenu: () => void;
+};
+
+export default function useMobileMenu(): UseMobileMenuType {
   const { isMobileMenuOpened, setIsMobileMenuOpened } = useContext(NavbarContext);
 
   const openMobileMenu = useCallback(() => setIsMobileMenuOpened(true), [
