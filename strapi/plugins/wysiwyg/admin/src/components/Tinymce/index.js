835fda0cc36701977d2c6f11442d8e688ea21434
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-03 00:45:45 +0700

    refactor: :recycle: remove verbose log

--- strapi/plugins/wysiwyg/admin/src/components/Tinymce/index.js
+++ strapi/plugins/wysiwyg/admin/src/components/Tinymce/index.js
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Editor } from "@tinymce/tinymce-react";
 
 const API_KEY = process.env.APP_TINY_MCE_KEY || "";
 
 const TinyEditor = ({ onChange, name, value }) => {
-  useEffect(() => {
-    console.log("API_KEY", API_KEY);
-  }, []);
-
   return (
     <Editor
       apiKey={API_KEY}
