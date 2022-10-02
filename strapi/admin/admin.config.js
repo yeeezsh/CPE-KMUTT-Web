10dbe36cd3aeabee6ddd146c83fc461dfe627051
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-03 00:41:25 +0700

    refactor: :recycle: remove verbose log

--- strapi/admin/admin.config.js
+++ strapi/admin/admin.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   webpack: (config, webpack) => {
     console.log("Using custom webpack plugin");
-    console.log("ENV", process.env);
 
     config.plugins.push(
       new webpack.DefinePlugin({
