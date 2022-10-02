222b8f97ac436db96f0c9410c2b1469615ab8da6
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-02 23:03:31 +0700

    fix: :ambulance: bad env config

--- strapi/plugins/wysiwyg/admin/admin.config.js
+++ strapi/plugins/wysiwyg/admin/admin.config.js
@@ -2,7 +2,9 @@ module.exports = {
   webpack: (config, webpack) => {
     config.plugins.push(
       new webpack.DefinePlugin({
-        APP_TINY_MCE_KEY: JSON.stringify(process.env.APP_TINY_MCE_KEY),
+        "process.env": {
+          APP_TINY_MCE_KEY: JSON.stringify(process.env.APP_TINY_MCE_KEY),
+        },
       })
     );
     return config;
