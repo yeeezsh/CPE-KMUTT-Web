b50744bba93327cd01cae8a206702a64b16e8480
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-02 23:39:58 +0700

    add log when build

--- strapi/admin/admin.config.js
+++ strapi/admin/admin.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   webpack: (config, webpack) => {
+    console.log("Using custom webpack plugin");
+
+    console.log("ENV");
+    console.log("APP_TINY_MCE_KEY", process.env.APP_TINY_MCE_KEY);
     config.plugins.push(
       new webpack.DefinePlugin({
         "process.env": {
