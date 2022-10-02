28515e6e985215f81899e88ba6ef0b26782ba57c
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-03 00:14:35 +0700

    test

--- strapi/admin/admin.config.js
+++ strapi/admin/admin.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   webpack: (config, webpack) => {
     console.log("Using custom webpack plugin");
+    console.log("ENV", process.env);
 
-    console.log("ENV");
-    console.log("APP_TINY_MCE_KEY", process.env.APP_TINY_MCE_KEY);
     config.plugins.push(
       new webpack.DefinePlugin({
         "process.env": {
