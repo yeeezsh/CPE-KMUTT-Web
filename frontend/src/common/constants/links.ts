b8c7cda92d8a22465ab7897ecb530ca6fd670a96
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-31 22:19:38 +0700

    feat: :sparkles: add /programs

--- frontend/src/common/constants/links.ts
+++ frontend/src/common/constants/links.ts
@@ -5,12 +5,15 @@ export const STATIC_ABOUT_US_LINK = '/about-us';
 export const STATIC_CONTACT_US = '/contact-us';
 export const STATIC_ABOUT_US = '/about-us';
 export const STATIC_STAFFS = '/staffs';
+export const STATIC_PROGRAMS = '/programs';
 
 export const STATIC_NEWS_CATEGORY_ID_LINK = '/news/category/[seo_link]';
 export const STATIC_NEWS_ID_LINK = '/news/[id]';
 export const STATIC_STAFF_ID_LINK = '/staff/[id]';
+export const STATIC_PROGRAMS_ID_LINK = '/programs/[id]';
 
 export const STATIC_NEWS_CATEGORY_ID_LINK_PATTERN = '[seo_link]';
 export const STATIC_NEWS_ID_LINK_PATTERN = '[id]';
+export const STATIC_PROGRAMS_ID_LINK_PATTERN = '[id]';
 
 export const DEFAULT_STATIC_NEWS_CATEGORY_QUERY = 'all';
---
+++ frontend/src/pages/programs/index.tsx
@@ -0,0 +1,5 @@
+import BrowseProgramPage from 'modules/programs/pages/BrowseProgramPage';
+
+// const mock: React.FC = () => <></>;
+
+export default BrowseProgramPage;
