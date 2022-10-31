08c526399325d4abc78c50a1b2926147012a664c
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-31 21:09:58 +0700

    test: :white_check_mark: update snapshot & fix dependency

--- frontend/src/common/components/Navbar/components/NavbarMenu/index.spec.tsx
+++ frontend/src/common/components/Navbar/components/NavbarMenu/index.spec.tsx
@@ -1,14 +1,21 @@
+import 'jest-styled-components';
+
 import React from 'react';
 
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
 
-import 'jest-styled-components';
+import { store } from 'common/stores';
 
 import NavbarMenu from '.';
 
 describe('Navbar Tests', () => {
   it('Navbar menu should be defined', () => {
-    const wrapper = shallow(<NavbarMenu />);
+    const wrapper = shallow(
+      <Provider store={store}>
+        <NavbarMenu />
+      </Provider>,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
