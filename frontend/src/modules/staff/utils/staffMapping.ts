43c357d7aa8d24fd77ad673969c5a81072d941e8
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-10-31 23:04:11 +0700

    feat: :sparkles: support staff ordering

--- frontend/src/modules/staff/utils/staffMapping.ts
+++ frontend/src/modules/staff/utils/staffMapping.ts
@@ -13,23 +13,34 @@ export function staffMapping(data: GetStaffsQuery): StaffGroups[] {
   const groups = data.staffsConnection?.groupBy?.academic_position_group?.map(
     (e) => e?.key,
   ) as string[] | undefined;
-  const staffs = data.staffs?.map(
-    (s) =>
-      ({
-        id: s?.id,
-        title: s?.title,
-        fullTitle: s?.full_title,
-        name: s?.name,
-        imageUrl: ImageStrapiUrl(s?.profile_image?.url || ''),
-        academicPosition: s?.academic_position,
-        academicPositionGroup: s?.academic_position_group,
-        phone: s?.phone_number,
-        email: s?.email,
-      } as Staff),
-  );
-  const mappedGroup = groups?.map((g) => ({
-    title: g,
-    staffs: staffs?.filter((s) => s?.academicPositionGroup === g),
-  })) as StaffGroups[];
+  const staffs = data.staffs
+    ?.map(
+      (s) =>
+        ({
+          id: s?.id,
+          title: s?.title,
+          fullTitle: s?.full_title,
+          name: s?.name,
+          imageUrl: ImageStrapiUrl(s?.profile_image?.url || ''),
+          academicPosition: s?.academic_position,
+          academicPositionGroup: s?.academic_position_group,
+          phone: s?.phone_number,
+          email: s?.email,
+          order: s?.order,
+        } as Staff),
+    )
+    .sort((a, b) => a.order - b.order);
+
+  const mappedGroup = groups
+    ?.map((g) => ({
+      title: g,
+      staffs: staffs?.filter((s) => s?.academicPositionGroup === g),
+      order: Math.min(
+        ...(staffs?.filter((s) => s?.academicPositionGroup === g).map((s) => s.order) || [
+          0,
+        ]),
+      ),
+    }))
+    .sort((a, b) => a.order - b.order) as StaffGroups[];
   return mappedGroup;
 }
