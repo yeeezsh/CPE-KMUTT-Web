1e9174b90531a89edcf82b2f1a29a8fbbf003d8e
kthuswongsa <korbboon.thuswongsa@agoda.com>
2022-11-07 01:11:41 +0700

    feat: :sparkles: add link on faqbanner

--- frontend/src/common/components/FaqBanner/index.tsx
+++ frontend/src/common/components/FaqBanner/index.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 
+import { useRouter } from 'next/router';
 import { HiOutlineArrowRight } from 'react-icons/hi';
 
 import { Btn } from 'common/components/Button/styled';
+import { STATIC_CONTACT_US } from 'common/constants/links';
 import baseUrl from 'common/utils/baseUrl';
 
 import { HOME_CONTENTS } from 'modules/home/constants';
@@ -11,28 +13,35 @@ import { Container, Row, Column, Header, Content, Button } from './styled';
 
 const ICON_SIZE = '16px';
 
-const FaqBanner: FC = () => (
-  <Container>
-    <Row className="align-center">
-      <Column>
-        <Header>{HOME_CONTENTS.question.header}</Header>
-        <Content>{HOME_CONTENTS.question.content}</Content>
-        <Button>
-          <Btn $color={'transparent'}>
-            ติดต่อเรา
-            <HiOutlineArrowRight
-              className="Icon"
-              style={{ marginLeft: '16px' }}
-              size={ICON_SIZE}
-            />
-          </Btn>
-        </Button>
-      </Column>
-      <Column>
-        <img src={baseUrl(HOME_CONTENTS.question.background)} alt="" />
-      </Column>
-    </Row>
-  </Container>
-);
+const FaqBanner: FC = () => {
+  const router = useRouter();
+
+  function onClickLink() {
+    router.push(STATIC_CONTACT_US);
+  }
+  return (
+    <Container>
+      <Row className="align-center">
+        <Column>
+          <Header>{HOME_CONTENTS.question.header}</Header>
+          <Content>{HOME_CONTENTS.question.content}</Content>
+          <Button onClick={onClickLink}>
+            <Btn $color={'transparent'}>
+              ติดต่อเรา
+              <HiOutlineArrowRight
+                className="Icon"
+                style={{ marginLeft: '16px' }}
+                size={ICON_SIZE}
+              />
+            </Btn>
+          </Button>
+        </Column>
+        <Column>
+          <img src={baseUrl(HOME_CONTENTS.question.background)} alt="" />
+        </Column>
+      </Row>
+    </Container>
+  );
+};
 
 export default FaqBanner;
